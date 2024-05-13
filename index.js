4ac687f99d9ca1bd415836a0ec8ecbe5d5bcda3c
efghuygrehirge <hsjcesenovelxan@hotmail.com>
2024-05-13 15:43:45 +0700

    aptos sdk

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-const bubbleSortOptimized = (arr) => {
-  let swapped;
-  do {
-    swapped = false;
-    for (let i = 0; i < arr.length - 1; i++) {
-      if (arr[i] > arr[i + 1]) {
-        [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
-        swapped = true;
+function isAlienSorted(words, order) {
+  const dict = new Map();
+  for (let i = 0; i < order.length; i++) {
+    dict.set(order[i], i);
+  }
+  for (let i = 0; i < words.length - 1; i++) {
+    const word1 = words[i];
+    const word2 = words[i + 1];
+    let found = false;
+    for (let j = 0; j < Math.min(word1.length, word2.length); j++) {
+      if (word1[j] !== word2[j]) {
+        if (dict.get(word1[j]) > dict.get(word2[j])) return false;
+        found = true;
+        break;
       }
     }
-  } while (swapped);
-  return arr;
-};
+    if (!found && word1.length > word2.length) return false;
+  }
+  return true;
+}
